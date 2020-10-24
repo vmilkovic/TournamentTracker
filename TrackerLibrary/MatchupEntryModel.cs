7787925c19520902512494a12bebe1ac2cf60a93
Vedran Milković <vedran.milkovic25@gmail.com>
2020-10-24 14:36:54 +0200

    Add project files.

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary {
+
+    /// <summary>
+    /// Represents one team in a matchup.
+    /// </summary>
+    public class MatchupEntryModel {
+
+        /// <summary>
+        /// Represents one team in the matchup.
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents the score of this particular team.
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents the matchup that this team came 
+        /// from as the winner.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
---
+++ TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary {
+    public class PrizeModel {
+        public int PlaceNumber { get; set; }
+        public string PlaceName { get; set; }
+        public decimal PlaceAmount { get; set; }
+        public double PlacePercentage { get; set; }
+    }
+}
